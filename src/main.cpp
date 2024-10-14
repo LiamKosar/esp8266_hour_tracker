7abfeeae7c6d6fc4c9653603e9ba165019ebd09c
Liam Kosar <liamkosar@Liams-MacBook-Pro.local>
2024-10-14 15:42:36 -0400

    Fix: adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,17 @@ int motion_detected = 0;
 unsigned long total_active_time = 0;
 unsigned long previous_total_active_time = 0;
 
+// Vibration sensor reading delay (ms)
 int reading_delay = 5000;
+
+//EEPROM write delay (ms)
 int write_delay = 60000;
 
 // put function declarations here:
 void takeFiveReadings();
 void readActiveTime();
 void writeActiveTime();
-void updateEEPRON();
+void updateEEPROM();
 
 void setup() {
   readActiveTime();
@@ -23,10 +26,13 @@ void setup() {
 
 void loop() {
   takeFiveReadings();
-  updateEEPRON();
+  updateEEPROM();
   delay(reading_delay);
 }
 
+/*Takes 5 readings, one every 50ms
+  If any of them is a 1 (vibration detected), assume that the next period (reading_delay),
+  is all vibration (running time)*/
 void takeFiveReadings() {
 
   motion_detected = 0;
@@ -36,6 +42,7 @@ void takeFiveReadings() {
     delay(reading_delay/50);
   }
 
+  // If there is motion detected, add the interval to total running time
   if (motion_detected == 1) {
     total_active_time += reading_delay + ((reading_delay/50) * 5);
   }
@@ -45,6 +52,10 @@ void takeFiveReadings() {
 
 }
 
+/*
+* Reads the value for total run time from EEPROM
+* Multiply by 1000, because the value in EEPROM is in seconds
+*/
 void readActiveTime() {
   EEPROM.begin(4);
   EEPROM.get(0,total_active_time);
@@ -54,6 +65,11 @@ void readActiveTime() {
   EEPROM.end();
 }
 
+/**
+ * Writes the total active time to EEPROM
+ * Converts to seconds (/1000) before storing
+ * to save space
+*/
 void writeActiveTime() {
   EEPROM.begin(4);
   EEPROM.put(0, total_active_time/1000);
@@ -61,7 +77,12 @@ void writeActiveTime() {
   EEPROM.end();
 }
 
-void updateEEPRON() {
+/**
+ * Updates the total running time in EEPROM
+ * Only if the difference between the current total time
+ * and the previously 'written' total time is more than the write delay
+*/
+void updateEEPROM() {
   if ((total_active_time - previous_total_active_time) >= (unsigned long)write_delay) {
     writeActiveTime();
     readActiveTime();
